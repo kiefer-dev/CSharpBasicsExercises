0bec0fcbb152918e608f575c6cd4826bfbc634c4
Kiefer Weis <kieferweis@protonmail.com>
2022-10-07 18:02:46 -0400

    Added exercise solution for lesson 49

--- 49.cs
+++ 49.cs
@@ -1,3 +1,4 @@
+// 1- Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
 namespace CSharpFundamentals
 {
     class Program
@@ -13,6 +14,34 @@ namespace CSharpFundamentals
             } else {
                 Console.WriteLine("Invalid");
             }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// 2- Write a program which takes two numbers from the console and displays the maximum of the two.
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers, and I'll tell you which one is bigger!");
+            Console.WriteLine("First Number:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second Number:");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} is bigger!", a);
+            } else if (b > a) { 
+                Console.WriteLine("{0} is bigger!", b);
+            } else { 
+                Console.WriteLine("{0} and {1} are the same!", a, b);
+            }
+
             Console.ReadLine();
         }
     }
