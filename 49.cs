33b2461c9ca13e0de9245937bfad7907f930356d
Kiefer Weis <kieferweis@protonmail.com>
2022-10-07 18:40:50 -0400

    Added exercise solution for lesson 49

--- 49.cs
+++ 49.cs
@@ -73,3 +73,31 @@ namespace CSharpFundamentals
         }
     }
 }
+
+
+// 4- Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. If the user enters a value less than the speed limit, program should display Ok on the console. If the value is above the speed limit, the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended.
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Speed Limit:");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Car speed:");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed <= speedLimit)
+            {
+                Console.WriteLine("Ok");
+            } else {
+                var points = Math.Floor((float)(carSpeed - speedLimit) / 5);
+                Console.WriteLine("{0} points on your record!", points);
+                if (points > 12)
+                    Console.WriteLine("License Suspended");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
